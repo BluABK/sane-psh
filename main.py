fdabd1e5361e2de1eb171ba0866a9fa935ed60fe
BluABK <BluABK@users.noreply.github.com>
2020-05-01 19:37:08 +0200

    Split route function code into multiple functions.

--- main.py
+++ main.py
@@ -8,6 +8,55 @@ import os
 app = Flask(__name__)
 
 
+def handle_get(req):
+    topic = request.args["hub.topic"]
+    challenge = request.args["hub.challenge"]
+    mode = request.args["hub.mode"]
+    if 'hub.lease_seconds' in request.args:
+        lease_seconds = request.args["hub.lease_seconds"]
+    else:
+        lease_seconds = ""
+    if 'hub.verify_token' in request.args:
+        verify_token = request.args['hub.verify_token']
+        # retv = verify_token
+
+    print("topic: {}\n"
+          "challenge: {}\n"
+          "mode: {}\n"
+          "lease_seconds: {}".format(topic, challenge, mode, lease_seconds))
+
+    return challenge
+
+
+def handle_deleted_entry(xml):
+    pass
+
+
+def handle_video(xml):
+    links = [{'href': lnk.get('href'), 'rel': lnk.get('rel')} for lnk in xml.feed.find_all('link')]
+    title = xml.feed.title.string
+    updated = xml.feed.updated.string
+    entry = {
+        'id': xml.feed.entry.id.string,
+        'video_id': xml.feed.entry.find('yt:videoId').string,
+        'channel_id': xml.feed.entry.find('yt:channelId').string,
+        'title': xml.feed.entry.title.string,
+        'links': [{'href': lnk.get('href'), 'rel': lnk.get('rel')} for lnk in xml.feed.entry.find_all('link')],
+        'channel_title': xml.feed.entry.author.name,
+        'channel_uri': xml.feed.entry.author.uri.string,
+        'published': xml.feed.entry.published.string,
+        'updated': xml.feed.entry.updated.string
+    }
+
+    print("\n")
+    print("Links: {links}".format(links=links))
+    print("Title: {title}".format(title=title))
+    print("Updated: {updated}".format(updated=updated))
+    print("Entry: :")
+    for key, value in entry.items():
+        print("\t{key}: {value}".format(key=key, value=value))
+
+
 @app.route('/sane-psh/', methods=['GET', 'POST'])
 def psh():
     retv = ''
@@ -31,47 +80,13 @@ def psh():
         with open('dumps/request_at_{}.xml'.format(datetime_stamp), 'w') as f:
             f.write(xml.feed.prettify())
 
-        links = [{'href': lnk.get('href'), 'rel': lnk.get('rel')} for lnk in xml.feed.find_all('link')]
-        title = xml.feed.title.string
-        updated = xml.feed.updated.string
-        entry = {
-            'id': xml.feed.entry.id.string,
-            'video_id': xml.feed.entry.find('yt:videoId').string,
-            'channel_id': xml.feed.entry.find('yt:channelId').string,
-            'title': xml.feed.entry.title.string,
-            'links': [{'href': lnk.get('href'), 'rel': lnk.get('rel')} for lnk in xml.feed.entry.find_all('link')],
-            'channel_title': xml.feed.entry.author.name,
-            'channel_uri': xml.feed.entry.author.uri.string,
-            'published': xml.feed.entry.published.string,
-            'updated': xml.feed.entry.updated.string
-        }
-
-        print("\n")
-        print("Links: {links}".format(links=links))
-        print("Title: {title}".format(title=title))
-        print("Updated: {updated}".format(updated=updated))
-        print("Entry: :")
-        for key, value in entry.items():
-            print("\t{key}: {value}".format(key=key, value=value))
+        if xml.feed.find('at'):
+            retv = handle_deleted_entry(xml)
+        else:
+            retv = handle_video(xml)
 
     if request.method == 'GET':
-        topic = request.args["hub.topic"]
-        challenge = request.args["hub.challenge"]
-        mode = request.args["hub.mode"]
-        if 'hub.lease_seconds' in request.args:
-            lease_seconds = request.args["hub.lease_seconds"]
-        else:
-            lease_seconds = ""
-        if 'hub.verify_token' in request.args:
-            verify_token = request.args['hub.verify_token']
-            # retv = verify_token
-
-        print("topic: {}\n"
-              "challenge: {}\n"
-              "mode: {}\n"
-              "lease_seconds: {}".format(topic, challenge, mode, lease_seconds))
-
-        retv = challenge
+        retv = handle_get(request)
 
     return make_response(retv, 200)
 
