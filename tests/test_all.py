7aa7ed57c21100c7b217d2d0300d925a22578ce9
BluABK <BluABK@users.noreply.github.com>
2020-05-02 17:21:13 +0200

    Added unit test for subscribe and unsubscribe GET requests.

--- tests/test_all.py
+++ tests/test_all.py
@@ -3,12 +3,15 @@ import unittest
 from database import init_db
 from tests.test_published_video import TestPublishedVideo
 from tests.test_deleted_entry import TestDeletedEntry
+from tests.test_subscription_request import TestSubscriptionRequest
 
 
 def suite():
     my_suite = unittest.TestSuite()
     my_suite.addTest(TestPublishedVideo('test_published_video'))
     my_suite.addTest(TestDeletedEntry('test_deleted_entry'))
+    my_suite.addTest(TestSubscriptionRequest('test_subscribe_request'))
+    my_suite.addTest(TestSubscriptionRequest('test_unsubscribe_request'))
     return my_suite
 
 
---
+++ tests/test_subscription_request.py
@@ -0,0 +1,90 @@
+import os
+import unittest
+
+from werkzeug.datastructures import ImmutableMultiDict
+
+from database import init_db
+from database.operations import get_channel
+from main import handle_get
+
+XML_FILEPATH = 'dumps/published_video.xml'
+
+
+class FakeGetRequest:
+    def __init__(self, args: ImmutableMultiDict):
+        self.args = args
+
+
+class TestSubscriptionRequest(unittest.TestCase):
+    cb_dict = {}
+    channel_id = "UCLozjflf3i84bu_2jLTK2rA"
+
+    def assert_dict(self, test_dict, correct_dict):
+        self.assertEqual(len(test_dict), len(correct_dict))
+
+        for key in test_dict:
+            if type(test_dict[key]) is dict:
+                self.assert_dict(test_dict[key], correct_dict[key])
+            else:
+                self.assertEqual(test_dict[key], correct_dict[key])
+
+    def test_subscribe_request(self):
+        expected_result = {
+            "channel_id": self.channel_id,
+            "subscribed": True,
+            "verify_token": None,
+            "hmac_secret": None
+        }
+
+        req = FakeGetRequest(
+            args=ImmutableMultiDict(
+                [('hub.topic', 'https://www.youtube.com/xml/feeds/videos.xml?channel_id=UCLozjflf3i84bu_2jLTK2rA'),
+                 ('hub.challenge', '3613557208738996482'), ('hub.mode', 'subscribe'), ('hub.lease_seconds', '432000')])
+        )
+
+        handle_get(req)
+
+        test_result = get_channel(self.channel_id)
+        relevant_test_results = {
+            "channel_id": test_result["channel_id"],
+            "subscribed": test_result["subscribed"],
+            "verify_token": test_result["verify_token"],
+            "hmac_secret": test_result["hmac_secret"]
+        }
+
+        self.assert_dict(relevant_test_results, expected_result)
+
+    def test_unsubscribe_request(self):
+        expected_result = {
+            "channel_id": self.channel_id,
+            "subscribed": False,
+            "verify_token": None,
+            "hmac_secret": None
+        }
+
+        req = FakeGetRequest(
+            args=ImmutableMultiDict(
+                [('hub.topic', 'https://www.youtube.com/xml/feeds/videos.xml?channel_id=UCLozjflf3i84bu_2jLTK2rA'),
+                 ('hub.challenge', '17674212813144385002'), ('hub.mode', 'unsubscribe')])
+        )
+
+        handle_get(req)
+
+        test_result = get_channel(self.channel_id)
+        relevant_test_results = {
+            "channel_id": test_result["channel_id"],
+            "subscribed": test_result["subscribed"],
+            "verify_token": test_result["verify_token"],
+            "hmac_secret": test_result["hmac_secret"]
+        }
+
+        self.assert_dict(relevant_test_results, expected_result)
+
+
+if __name__ == '__main__':
+    init_db()
+    # Workaround for unreliable CWD:
+    # When running test_all.py CWD is suddenly this dir, not project root,
+    # but when running this test stand-alone CWD is project root.
+    XML_FILEPATH = os.path.join('tests', XML_FILEPATH)
+    unittest.main()
