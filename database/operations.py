a757eeb21c4254646e0243ff47bad9f35b239c5c
BluABK <bluabk@bluabk.net>
2021-07-26 01:55:58 +0200

    Added DB ops: update_columns, add_or_update_video and experimental add_or_update_row.
    These functions are meant ot replace the old separate DB add and update functions that kept running into issues with duplicate entries due to DB not being committed in time for the next request.

--- database/operations.py
+++ database/operations.py
@@ -203,21 +203,109 @@ def update_channel(channel_id: str, **kwargs):
         session.close()
 
 
+def update_columns(base: Base, **kwargs):
+    """
+    Update DB row columns based on kwargs.
+
+    :param base: Database Base object.
+    :param kwargs: Columns
+    :return:
+    """
+    if hasattr(base, "last_modified"):
+        base.update_last_modified()
+
+    for attr, value in kwargs.items():
+        if hasattr(base, attr):
+            if getattr(base, attr) != value:
+                setattr(base, attr, value)
+
+
 def update_video(video_id: str, **kwargs):
     # Create a Session
     session = db_session()
     try:
         video = session.query(Video).filter(Video.video_id == video_id).one()
 
-        video.update_last_modified()
-
-        for attr, value in kwargs.items():
-            if hasattr(video, attr):
-                if getattr(video, attr) != value:
-                    setattr(video, attr, value)
+        update_columns(video, **kwargs)
 
         session.commit()
     except SQLAlchemyError:
+        log.exception(SQLAlchemyError)
+        session.rollback()
+        raise
+    finally:
+        session.close()
+
+
+def add_or_update_video(video_id, **kwargs):
+    """
+    Adds Video row to DB or updates existing one.
+    :return:
+    """
+    # Create a Session
+    session = db_session()
+    try:
+        # Check if entry already exists by getting the first item in the query list.
+        video_record: Union[Video, None] = session.query(Video).filter(Video.video_id == video_id).first()
+
+        if video_record:
+            log.db_info("Update Video '{video_id}'.".format(video_id=video_id))
+            video_dict = video_record.as_dict()
+            log.debug2(video_dict)
+            update_columns(video_record, **kwargs)
+        else:
+            log.db_info("Add Video '{video_id}'.".format(video_id=video_id))
+            video = Video(video_id=video_id, **kwargs)
+            session.add(video)
+
+        session.commit()
+    except SQLAlchemyError:
+        log.exception(SQLAlchemyError)
+        session.rollback()
+        raise
+    finally:
+        session.close()
+
+
+def add_or_update_row(base: Union[Video, Channel], filter_criterion, **kwargs):
+    """
+    Adds Video/Channel row to DB or updates existing one.
+
+    NB: **EXPERIMENTAL**
+    :return:
+    """
+    # Create a Session
+    session = db_session()
+    try:
+        # Check if entry already exists by getting the first item in the query list.
+        if isinstance(base, Video):
+            record: Union[Base, None] = session.query(Video).filter_by(video_id=filter_criterion).first()
+        elif isinstance(base, Channel):
+            record: Union[Base, None] = session.query(Channel).filter_by(channel_id=filter_criterion).first()
+        else:
+            raise SQLAlchemyError("add_or_update_row called with unsupported Base '{basename}'".format(
+                basename=base.__tablename__))
+
+        if record:
+            log.db_info("Update {basename} '{flt}'.".format(basename=base.__tablename__, flt=filter_criterion))
+            record_dict = record.as_dict()
+            log.debug2(record_dict)
+            update_columns(record, **kwargs)
+        else:
+            log.db_info("Add {basename} '{flt}'.".format(basename=base.__tablename__, flt=filter_criterion))
+            if isinstance(base, Video):
+                row = Video(**kwargs)
+            elif isinstance(base, Channel):
+                row = Channel(**kwargs)
+            else:
+                raise SQLAlchemyError("add_or_update_row called with unsupported Base '{basename}'".format(
+                    basename=base.__tablename__))
+
+            session.add(row)
+
+        session.commit()
+    except SQLAlchemyError:
+        log.exception(SQLAlchemyError)
         session.rollback()
         raise
     finally:
